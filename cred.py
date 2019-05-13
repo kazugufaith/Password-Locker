9d327a6a4588f895649c6a053580ff8d1dc73805
kazugufaith <faithmugesia@gmail.com>
2019-05-13 13:06:44 +0300

    added view-account and delete account function on cred.py file

--- cred.py
+++ cred.py
@@ -9,24 +9,24 @@ class Credential:
         Credential.credential_list.append(
             {'account': self.account, 'username': self.username, 'password': self.password})
 
-    # def save_account(self):
-    #     '''
-    #     save_account method saves account objects into credential_list
-    #     '''
-    #
-    #     Credential.credential_list.append(self)
-    #
-    # @classmethod
-    # def view_account(cls):
-    #     print('-' * 40)
-    #     print("Existing Accounts: ")
-    #     print('-' * 40)
-    #     print("Account Name || UserName || Password")
-    #     for i in cls.credential_list:
-    #         print(f" {i['account']} || {i['username']} || {i['password']}")
-    #
-    # @classmethod
-    # def delete_account(cls, account):
-    #     for a in cls.credential_list:
-    #         if a['account'] == account:
-    #             cls.credential_list.remove(a)
+    def save_account(self):
+        '''
+        save_account method saves account objects into credential_list
+        '''
+
+        Credential.credential_list.append(self)
+
+    @classmethod
+    def view_account(cls):
+        print('-' * 40)
+        print("Existing Accounts: ")
+        print('-' * 40)
+        print("Account Name || UserName || Password")
+        for i in cls.credential_list:
+            print(f" {i['account']} || {i['username']} || {i['password']}")
+
+    @classmethod
+    def delete_account(cls, account):
+        for a in cls.credential_list:
+            if a['account'] == account:
+                cls.credential_list.remove(a)
