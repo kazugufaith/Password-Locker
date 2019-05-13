17ae61bbf28bb7f03217aca3043ef6204e27c1cc
kazugufaith <faithmugesia@gmail.com>
2019-05-13 13:31:16 +0300

    added unittest.TestCase that helps in creating test cases on test.py file

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-# import unittest  # Importing the unittest module
-# from user import User  # Importing User class
-# from credential import Credential  # Importing Credential class
-#
-#
-# class TestUser(unittest.TestCase):
-#     '''
-#     Test class that defines test cases for the user class behaviours
-#     Args:
-#         unittest.TestCase: TestCase class that helps in creating test cases
-#     '''
-#
+import unittest  # Importing the unittest module
+from user import User  # Importing User class
+from credential import Credential  # Importing Credential class
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
 #     def setUp(self):
 #         '''
 #         Set up method to run before each test cases
